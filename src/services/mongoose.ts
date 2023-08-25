6725fe30d8485d4642846035f0c76c02438822e2
ElF4TboY <thomasmarchand63@gmail.com>
2023-08-25 16:26:55 +0200

    add .env

--- src/services/mongoose.ts
+++ src/services/mongoose.ts
@@ -1,8 +1,7 @@
+import "dotenv/config";
 import { connect } from "mongoose";
 
-const uri: string =
-;
-
+const uri: any = process.env.MONGO_URL;
 export const connectDB = async () => {
   await connect(uri);
   console.log("DB connecting");
