009cb67b0e4dc8027588da40ce70f37f1d55ce64
ElF4TboY <thomasmarchand63@gmail.com>
2023-08-26 11:48:00 +0200

    add index html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 import express from "express";
 
 import { connectDB } from "./src/services/mongoose.js";
@@ -8,9 +10,15 @@ import students from "./src/routes/students.routes.js";
 
 const app = express();
 const port = 3000;
+const dir = dirname(fileURLToPath(import.meta.url));
+const file = join(dir, "./src/view/index.html");
 
 connectDB().catch((err) => console.log(err));
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+  await res.sendFile(file);
+});
 
 app.use("/classrooms", classrooms);
 app.use("/noteBoard", noteBoard);
